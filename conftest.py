9442e70822c9d0541c3502712fd6b07ee8d99629
Alexander Strateichuk <a.strateichuk@gmail.com>
2018-09-15 00:09:35 +0300

    FTP fixture

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import json
 import pytest
 import os.path
+import ftputil
 
 
 from fixture.application import Application
@@ -17,16 +18,17 @@ def load_config(file):
     return target
 
 @pytest.fixture
-def app(request):
+def app(request, config):
     global fixture
     global target
     browser = request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--target"))
-
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, config=web_config)
-
+        fixture = Application(browser=browser, config=config)
     return fixture
+
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -36,6 +38,31 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            remote.remove("config_inc.php.bak")
+        if remote.path.isfile("config_inc.php"):
+            remote.rename("config_inc.php", "config_inc.php.bak")
+        remote.upload(os.path.join(os.path.dirname(__file__),"resources/config_inc.php"), "config_inc.php" )
+
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            if remote.path.isfile("config_inc.php"):
+                remote.remove("config_inc.php")
+            remote.rename("config_inc.php.bak", "config_inc.php")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def configure_server(request, config):
+    install_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    def fin():
+        restore_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    request.addfinalizer(fin)
 
 
 def pytest_addoption(parser):
